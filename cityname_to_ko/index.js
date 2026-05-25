650d65431d75a7d8284ac2ad9293f926057df1aa
Iranto <justavojo@icloud.com>
2026-05-25 20:01:27 +0900

    2026-05-25

--- cityname_to_ko/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    alphabet:       require('./lib/alphabet'),
-    romanize:       require('./lib/romanize'),
-    hangulify:      require('./lib/hangulify'),
-
-    assemble:       require('./lib/assemble'),
-    disassemble:    require('./lib/disassemble'),
-};
